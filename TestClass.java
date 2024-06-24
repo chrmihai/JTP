c7797e1e63afae5d0f3e27ee791dd9bc4f67a1e4
chrmihai <chrmihai@gmail.com>
2024-06-24 11:18:09 +0300

    modified value of b

--- TestClass.java
+++ TestClass.java
@@ -2,7 +2,7 @@ public class TestClass {
 
     public static void main(String[] args) {
         int a = 5;
-        int b = 5;
+        int b = 7;
         int sum = a+b;
 
         System.out.println("a+b = " + sum);
